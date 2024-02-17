b7cff7e876cf29b5274f95dade46cf603dc70d6f
Victor Casado <vcasado50@stuy.edu>
2024-02-17 12:28:02 -0500

    condensed getting inputs into fxns

--- Driver.java
+++ Driver.java
@@ -3,101 +3,59 @@ import java.util.Scanner;
 public class Driver{
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter your name: ");
-        String name = input.nextLine();
-
-        double GPA = -1;
-        boolean goodInput = false;
-        while(!goodInput){
-            try{
-                System.out.print("Enter your GPA: ");
-                GPA = Double.parseDouble(input.nextLine());
-                if (GPA >= 0 && GPA <= 5){
-                    goodInput = true;
-                }
-                else{
-                    System.out.println("please enter a valid GPA (from 0.0 to 5.0)");
-                }
-            }
-            catch(Exception e){
-                System.out.println("please enter a valid GPA (from 0.0 to 5.0)");
-            }
-        }
-
-        double ACT = -1;
-        goodInput = false;
-        while(!goodInput){
-            try{
-                System.out.print("Enter your ACT: ");
-                ACT = Double.parseDouble(input.nextLine());
-                if (ACT >= 0 && ACT <= 36){
-                    goodInput = true;
-                }
-                else{
-                    System.out.println("please enter a valid ACT (from 1.0 to 36.0)");
-                }
-            }
-            catch(Exception e){
-                System.out.println("please enter a valid ACT (from 1.0 to 36.0)");
-            }
-        }
-
-        int SAT = -1;
-        goodInput = false;
-        while(!goodInput){
-            try{
-                System.out.print("Enter your SAT: ");
-                SAT = Integer.parseInt(input.nextLine());
-                if (SAT >= 400 && SAT <= 1600){
-                    goodInput = true;
-                }
-                else{
-                    System.out.println("please enter a valid SAT (from 400 to 1600)");
-                }
-            }
-            catch(Exception e){
-                System.out.println("please enter a valid ACT (from 400 to 1600)");
-            }
-        }
-
-        int essayStrength = -1;
-        goodInput = false;
-        while(!goodInput){
-            try{
-                System.out.print("Enter your Essay Strength (0-100): ");
-                essayStrength = Integer.parseInt(input.nextLine());
-                if (essayStrength >= 0 && essayStrength <= 100){
-                    goodInput = true;
-                }
-                else{
-                    System.out.println("please enter a valid Essay Strength (from 0 to 100)");
-                }
-            }
-            catch(Exception e){
-                System.out.println("please enter a valid Essay Strength (from 0 to 100)");
-            }
-        }
-
-        int extracurricularStrength = -1;
-        goodInput = false;
-        while(!goodInput){
-            try{
-                System.out.print("Enter your ExtraCurricular Strength (0-100): ");
-                extracurricularStrength = Integer.parseInt(input.nextLine());
-                if (extracurricularStrength >= 0 && extracurricularStrength <= 100){
-                    goodInput = true;
-                }
-                else{
-                    System.out.println("please enter a valid ExtraCurricular Strength (from 0 to 100)");
-                }
-            }
-            catch(Exception e){
-                System.out.println("please enter a valid ExtraCurricular Strength (from 0 to 100)");
-            }
-        }
+        String name = getStringInput(name, input);
+        double GPA = getDoubleInput("GPA", input, 0.0, 5.0);
+        double ACT = getDoubleInput("ACT", input, 1.0, 36.0);
+        int SAT = getIntegerInput("SAT", input, 400, 1600);
+        int essayStrength = getIntegerInput("Essay Strength", input, 0, 100);
+        int extracurricularStrength = getIntegerInput("ExtraCurricular Strength", input, 0, 100);
 
         Student user = new Student(name, GPA, ACT, SAT, essayStrength, extracurricularStrength);
         System.out.println("--------------------------");
         System.out.println(user);
+    }
+    public String getStringInput(String inputName, scanner input){
+        System.out.println("Enter your " + inputName + ": ");
+        return input.nextLine;
+    }
+    public double getDoubleInput(String inputName, scanner input, double min, double max){
+        double output = min - 1;
+        boolean goodInput = false;
+        while(!goodInput){
+            try{
+                System.out.print("Enter your " + inputName + ": ");
+                output = Double.parseDouble(input.nextLine());
+                if (output >= min && output <= max){
+                    goodInput = true;
+                }
+                else{
+                    System.out.println("please enter a valid " + inputName + " (from " + min + " to " + max + " )");
+                }
+            }
+            catch(Exception e){
+                System.out.println("please enter a valid " + inputName + " (from " + min + " to " + max + " )");
+            }
+        }
+        return output;
+    }
+    public double getIntegerInput(String inputName, scanner input, int min, int max){
+        int output = min - 1;
+        boolean goodInput = false;
+        while(!goodInput){
+            try{
+                System.out.print("Enter your " + inputName + ": ");
+                output = Integer.parseInt(input.nextLine());
+                if (output >= min && output <= max){
+                    goodInput = true;
+                }
+                else{
+                    System.out.println("please enter a valid " + inputName + " (from " + min + " to " + max + " )");
+                }
+            }
+            catch(Exception e){
+                System.out.println("please enter a valid " + inputName + " (from " + min + " to " + max + " )");
+            }
+        }
+        return output;
     }
 }
